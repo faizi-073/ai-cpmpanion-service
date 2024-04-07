7d2442aeaa7b9e553dde9efadc74c208d0aae6d8
faizan ali <iamfaizan073@gmail.com>
2024-04-07 16:59:50 +0500

    authentication system

--- ormconfig.js
+++ ormconfig.js
@@ -1,14 +1,13 @@
 module.exports = {
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'your_username',
-    password: 'your_password',
-    database: 'your_database_name',
-    entities: ['dist/**/*.entity{.ts,.js}'],
-    migrations: ['dist/migrations/*{.ts,.js}'],
-    cli: {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'your_username',
+  password: 'your_password',
+  database: 'your_database_name',
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  migrations: ['dist/migrations/*{.ts,.js}'],
+  cli: {
     migrationsDir: 'src/migrations',
-    },
-  };
- 
+  },
+};
